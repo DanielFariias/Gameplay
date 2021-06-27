77b9609e3f7365c705a2edfc1ae24a51ac7cf63f
danielfariias <danielfariias15@gmail.com>
2021-06-26 21:05:46 -0300

    App Completed

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { AppRoutes } from "./app.routes";
+import { SingIn } from "../screens/SignIn";
+import { useAuth } from "../hooks/auth";
+
+export function Routes() {
+  const { user } = useAuth()
+  return (
+    <NavigationContainer>
+      {user.id ? <AppRoutes /> : <SingIn />}
+    </NavigationContainer>
+  )
+}
